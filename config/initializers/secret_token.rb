0bd1eff3ad16aae5a4ceff6f1bd3d58bee541149
Alfredo Cobo <ajscobo@gmail.com>
2013-07-04 17:45:12 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streamsaster::Application.config.secret_token = '71dca2cf92d767b74abb69edc10589c56ab0be3a31ba88263f3e083b83de088a4474aaaf533c1c41ad7e9d4103b455086228d84ca4c1fa83992cd97731b5cf31'

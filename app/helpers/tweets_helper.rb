028724b475258812aed0fe9692f9cb008d55b3ad
Alfredo Cobo <ajscobo@gmail.com>
2013-08-05 21:43:52 -0400

    additional data

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -10,13 +10,17 @@ module TweetsHelper
 	    longitud = nil
 	    latitude = nil
 	  end
+	  retweet_count = status.retweet_count
 	  author = status.user
 	  user = author.screen_name
 	  location = author.location
 	  profile_image_url = author.profile_image_url
 	  created_at = status.created_at
+	  statuses_count = author.statuses_count
+	  friends_count = author.friends_count
 	  ::Tweet.new(native_id: id, text: status.text, coordinates: [longitude, latitude], 
-	                 user: user, location: location, profile_image_url: profile_image_url, created_at: created_at)
+	                 retweet_count: retweet_count, user: user, location: location, profile_image_url: profile_image_url, 
+	                 created_at: created_at, statuses_count: statuses_count, friends_count: friends_count)
 	end
 	
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,12 +8,17 @@ class Tweet
   field :native_id, type: String
   field :text, type: String
   field :coordinates, type: Array
+  field :retweet_count, type: Integer
 
   #User
   field :created_at, type: DateTime
   field :user, type: String
   field :location, type: String
   field :profile_image_url, type: String
+  field :followers_count, type: Integer
+  field :lang, type: String
+  field :statuses_count, type: Integer
+  field :friends_count, type: Integer
 
   #Validations
   validates_presence_of :native_id, :allow_nil => false
